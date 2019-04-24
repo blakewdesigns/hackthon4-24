af04f7f00aaf0552ccf4fbff4c5ed9079726acb1
Andrew Hager <scott.hager121@gmail.com>
2019-04-24 10:58:38 -0400

    Update global.js

--- global.js
+++
@@ -1,6 +0,0 @@
-const counter_button = document.getElementById("click_button")
-
-click_button.onclick = function () {
-    // Complete this function so that it increments the variable named count
-    document.querySelector('#countspan').innerHTML = ++count
-}
